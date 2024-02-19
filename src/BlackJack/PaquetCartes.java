0b13a6568fcad465d6fa587e302eb41c61b0866a
RdFatimazahra <fatimaezahraradi@gmail.com>
2024-02-19 16:22:03 +0100

    Methode Initialiser qui fait l'initialisation du paquet de carte

--- src/BlackJack/PaquetCartes.java
+++ src/BlackJack/PaquetCartes.java
@@ -3,5 +3,21 @@ import java.util.List;
 import java.util.Random;
 
 public class PaquetCartes {
-   
-}
+    private List<Carte> cartes;
+
+    public PaquetCartes() {
+        this.cartes = new ArrayList<>();
+        initialiserPaquet();
+        melangerCartes();
+    }
+
+    // Méthode pour initialiser le paquet de cartes avec 52 cartes
+    private void initialiserPaquet() {
+        for (int couleur = 1; couleur <= 4; couleur++) {
+            for (int valeur = 1; valeur <= 13; valeur++) {
+                cartes.add(new Carte(couleur, Math.min(valeur, 10)));
+            }
+        }
+    }
+
+  
