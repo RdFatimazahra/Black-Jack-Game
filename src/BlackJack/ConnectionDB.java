f147d38872e45fab27edd739068e5a3aff0b8dda
RdFatimazahra <fatimaezahraradi@gmail.com>
2024-02-25 17:43:35 +0100

    Connection JDBC

---
+++ src/BlackJack/ConnectionDB.java
@@ -0,0 +1,34 @@
+package BlackJack;
+
+import java.sql.connection;
+import java.sql.DriverManager;
+
+
+public class ConnectionDB {
+	
+
+	public static void main(String[] args) {
+
+		Connection connection = null;
+		
+		try {
+			
+			Class.forName("org.postgresql.Driver");
+			connection = DriverManager.getConnection("jdbc:postgresql://Localhost:5432/postgres","postgres","root");
+			if(connection!= null) {
+				System.println.out("Connection OK");
+			} else {
+				System.println.out("Connection Failed");
+				
+			}
+			} catch (Exception e) {
+				
+				system.println.out(e);
+				
+			}
+				
+		}
+		
+	}
+
+}
--- src/BlackJack/PaquetCartes.java
+++ src/BlackJack/PaquetCartes.java
@@ -15,7 +15,7 @@ public class PaquetCartes implements PaquetCartesInterface {
     
     @Override
     // Méthode pour initialiser le paquet de cartes avec 52 cartes
-    private void initialiserPaquet() {
+    public void initialiserPaquet() {
         for (int couleur = 1; couleur <= 4; couleur++) {
             for (int valeur = 1; valeur <= 13; valeur++) {
                 cartes.add(new Carte(couleur, Math.min(valeur, 10)));
@@ -25,7 +25,7 @@ public class PaquetCartes implements PaquetCartesInterface {
 
     @Override
  // Méthode pour mélanger les cartes dans le paquet
-    private void melangerCartes() {
+    public void melangerCartes() {
         Random random = new Random();
         for (int i = cartes.size() - 1; i > 0; i--) {
             int j = random.nextInt(i + 1);
